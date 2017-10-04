ea00ea656c187f0ba315b235d8d19454a2cde0a3
b4dnewz <filippo@codekraft.it>
2017-10-04 11:37:03 +0200

    Added check to load unminified script

--- generators/app/templates/plugin/include/class-main.php
+++ generators/app/templates/plugin/include/class-main.php
@@ -125,11 +125,15 @@ class <%= className %> {
   * @method plugin_enqueue_scripts
   */
   function plugin_enqueue_admin_scripts() {
+
+    $min = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+
     wp_register_style( '<%= projectName %>_admin_style', <%= definePrefix %>_DIR_URL . '/assets/dist/css/admin.css', array(), null );
-    wp_register_script( '<%= projectName %>_admin_script', <%= definePrefix %>_DIR_URL . '/assets/dist/js/admin/admin.min.js', array(), null, true );
-    wp_enqueue_script('jquery');
     wp_enqueue_style('<%= projectName %>_admin_style');
+
+    wp_register_script( '<%= projectName %>_admin_script', <%= definePrefix %>_DIR_URL . "/assets/dist/js/admin/admin{$min}.js", array('jquery'), null, true );
     wp_enqueue_script('<%= projectName %>_admin_script');
+
   }
 
   /**
@@ -137,11 +141,15 @@ class <%= className %> {
   * @method plugin_enqueue_scripts
   */
   function plugin_enqueue_scripts() {
+
+    $min = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+
     wp_register_style( '<%= projectName %>_user_style', <%= definePrefix %>_DIR_URL . '/assets/dist/css/user.css', array(), null );
-    wp_register_script( '<%= projectName %>_user_script', <%= definePrefix %>_DIR_URL . '/assets/dist/js/user/user.min.js', array(), null, true );
-    wp_enqueue_script('jquery');
     wp_enqueue_style('<%= projectName %>_user_style');
+
+    wp_register_script( '<%= projectName %>_user_script', <%= definePrefix %>_DIR_URL . "/assets/dist/js/user/user{$min}.js", array('jquery'), null, true );
     wp_enqueue_script('<%= projectName %>_user_script');
+
   }
 
   /**
