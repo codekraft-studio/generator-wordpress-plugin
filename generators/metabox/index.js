4fdd623e8901a23d08fa3fd78b23ff566e3c2fb2
b4dnewz <filippo@codekraft.it>
2017-11-02 00:05:38 +0100

    Metabox options close #5
    Now it asks for the basic add_metabox options, still need to see if is better to ask for screen or get the screen from the post argument in the function (like now)

--- generators/metabox/index.js
+++ generators/metabox/index.js
@@ -2,6 +2,7 @@
 
 const _ = require('lodash');
 const path = require('path');
+const chalk = require('chalk');
 const WPGenerator = require('../../common/generator');
 
 module.exports = class extends WPGenerator {
@@ -10,13 +11,36 @@ module.exports = class extends WPGenerator {
     super(args, opts);
   }
 
+  // Get specific submodule details
+  prompting() {
+    return this.prompt([{
+      type: 'input',
+      name: 'screen',
+      message: 'The screen or screens on which to show the box (leave blank for default screen).',
+      default: null
+    }, {
+      type: 'list',
+      name: 'context',
+      message: 'The context within the screen where the boxes should display:',
+      choices: ['advanced', 'normal', 'side'],
+      default: 0
+    }, {
+      type: 'list',
+      name: 'priority',
+      message: 'The priority within the context where the boxes should show:',
+      choices: ['default', 'high', 'low'],
+      default: 0
+    }]).then((answers) => {
+      _.assign(this.props, answers);
+    });
+  }
+
+  // Set specific properties
   configuring() {
     // Get the project defaults
     this.defaults();
-
     // The subgenerator name
     this.name = path.basename(__dirname);
-
     // Sub generator properties overrides
     this.props.id = _.snakeCase(this.options.name);
     this.props.title = _.startCase(this.options.name);
@@ -28,8 +52,8 @@ module.exports = class extends WPGenerator {
     super.writing()
   }
 
-  // Update the main class instance
-  end() {
+  // Used internally to dinamic update the main class
+  conflicts() {
 
     try {
 
@@ -57,6 +81,7 @@ module.exports = class extends WPGenerator {
 
       // Exit if entry is already in
       if( index > -1 ) {
+        this.log(chalk.cyan('identical'), `class name ${childClass} inside metaboxes array.`)
         return;
       }
 
@@ -90,12 +115,12 @@ module.exports = class extends WPGenerator {
         chalk.bold.yellow('to your plugin main class.')
       );
 
-    } finally {
-      // Call the parent end method
-      super.end();
     }
 
   }
 
+  end() {
+    super.end();
+  }
 
 };
