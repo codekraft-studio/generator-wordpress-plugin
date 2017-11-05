d39f85e228be136af39a837dbe0b271f3f181c4f
b4dnewz <filippo@codekraft.it>
2017-11-05 13:51:02 +0100

    Added support for dashwidgets

---
+++ __tests__/dashwidget.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const _ = require('lodash');
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+const subGenerator = path.basename(__filename, '.js');
+
+const nameInput = 'Test';
+
+const vars = {
+  "projectName": "my-plugin",
+  "projectVersion": "0.0.1",
+  "projectAuthor": "codekraft-studio",
+  "projectVersion": "0.0.1",
+  "projectLicense": "Apache-2.0"
+};
+
+describe('SubGenerator:widget', () => {
+  let generator;
+  beforeAll((done) => {
+    // Run the generator
+    generator = helpers.run(path.join(__dirname, `../generators/${subGenerator}`))
+      .withArguments([nameInput]);
+
+    // Use mock values
+    generator.on('ready', (generator) => {
+      generator.config.set(vars);
+      generator.config.save();
+    }).on('end', done);
+  });
+
+  // Default stuff that happen always
+  describe('default subgenerator tests', () => {
+    it('create subgenerator class file in directory named like subgenerator', () => {
+      assert.file([`include/${subGenerator}/class-test.php`]);
+    });
+
+    it('set the class name of the file as input plus subgenerator name', () => {
+      assert.fileContent(`include/${subGenerator}/class-test.php`, `class ${nameInput}_${_.capitalize(subGenerator)}`);
+    });
+  });
+});
