9ba61836b04dd3e08eb3969945fa19bf5a09c30b
Filippo Conti <filippo@codekraft.it>
2018-03-18 15:02:37 +0100

    Removed vendor folder and scripts

--- generators/app/templates/grunt/_Gruntfile.js
+++ generators/app/templates/grunt/_Gruntfile.js
@@ -63,20 +63,6 @@ module.exports = function (grunt) {
         files: {
           'assets/dist/js/admin/admin.min.js': ['<%= concat.adminScripts.dest %>']
         }
-      },
-      vendorScripts: {
-        options: {
-          sourceMap: false
-        },
-        files: [{
-          expand: true,
-          cwd: 'assets/src/js/vendor',
-          src: '**/*.js',
-          dest: 'assets/dist/js/vendor',
-          rename: function (dst, src) {
-            return dst + '/' + src.replace('.js', '.min.js');
-          }
-        }]
       }
     },
 
