61485db4463b21e54c0bac2790c44622d3556f40
Filippo Conti <filippo@codekraft.it>
2018-03-19 22:18:16 +0100

    Support for "nothing" as project manager
    Updated the main class template to handle different file system dispositions, updated banner

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
@@ -10,31 +11,105 @@ const prompts = {
   projectVersion: '1.0.0'
 };
 
-describe('SubGenerator:app', () => {
-  beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts(prompts)
-      .withOptions({
-        'skip-welcome-message': true,
-        'skip-message': true
+describe('Generator:app', () => {
+  // Default generator execution
+  describe('default options', () => {
+    beforeAll(() => helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts(prompts));
+
+    it('creates and move in a folder named like the projectName', () => {
+      assert.equal(path.basename(process.cwd()), 'my-plugin');
+    });
+
+    it('creates a project configuration file', () => {
+      assert.file('.yo-rc.json');
+    });
+
+    it('creates the main plugin class file', () => {
+      assert.file('include/class-main.php');
+    });
+
+    it('creates the plugin readme file', () => {
+      assert.file('readme.txt');
+    });
+
+    it('copy the plugin template dot files', (done) => {
+      fs.readdir(path.join(__dirname, '../generators/app/templates/plugin'), (err, files) => {
+        expect(err).toBeNull();
+        files.forEach(f => {
+          if (f.startsWith('.')) {
+            assert.file(f);
+          }
+        })
+        done();
       });
+    });
+
+    it('set the project details in the configuration file', () => {
+      assert.fileContent('.yo-rc.json', `"projectName": "${prompts.projectName}"`);
+      assert.fileContent('.yo-rc.json', `"projectTitle": "${prompts.projectTitle}"`);
+      assert.fileContent('.yo-rc.json', `"projectVersion": "${prompts.projectVersion}"`);
+    });
+
+    it('set the assets path', () => {
+      assert.fileContent('include/class-main.php', `/assets/dist/css/user.css`);
+      assert.fileContent('include/class-main.php', `/assets/dist/js/user/user{$min}.js`);
+    });
   });
 
-  it('creates and move in a folder named like the projectName', () => {
-    assert.equal(path.basename(process.cwd()), 'my-plugin');
+  // Custom prompts and options
+  describe('custom options: no project manager', () => {
+    let cPrompts = Object.assign({}, prompts, { projectManager: false });
+    let tmpDirectory;
+
+    // Run the generator
+    beforeAll(() => helpers.run(path.join(__dirname, '../generators/app'))
+      .inTmpDir(dir => { tmpDirectory = dir; })
+      .withPrompts(cPrompts));
+
+    it('wont create the project package file', () => {
+      assert.noFile('package.json');
+    });
+
+    it('wont create the package manager files', () => {
+      assert.noFile([
+        'gulpfile.js',
+        'Gruntfile.js'
+      ]);
+    });
+
+    it('copy the assets without src dir', () => {
+      assert.file([
+        'assets/css/user.css',
+        'assets/css/admin.css',
+        'assets/js/admin/admin.js',
+        'assets/js/user/user.js'
+      ]);
+    });
+
+    it('set the assets path', () => {
+      assert.fileContent('include/class-main.php', `/assets/css/user.css`);
+      assert.fileContent('include/class-main.php', `/assets/js/user/user{$min}.js`);
+    });
   });
 
-  it('creates the main plugin class file', () => {
-    assert.file(['include/class-main.php']);
-  });
-
-  it('creates a project configuration file', () => {
-    assert.file('.yo-rc.json');
-  });
-
-  it('set the project details in the configuration file', () => {
-    assert.fileContent('.yo-rc.json', `"projectName": "${prompts.projectName}"`);
-    assert.fileContent('.yo-rc.json', `"projectTitle": "${prompts.projectTitle}"`);
-    assert.fileContent('.yo-rc.json', `"projectVersion": "${prompts.projectVersion}"`);
+  // Using custom command line flags
+  describe('custom flags', () => {
+    it('disable git package with --no-git', (done) => {
+      let tmpDirectory;
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .inTmpDir(dir => { tmpDirectory = dir; })
+        .withPrompts(prompts)
+        .withOptions({
+          'git': false
+        })
+        .then(() => {
+          fs.stat(path.join(tmpDirectory, '.git'), (err, stats) => {
+            expect(err).toBeDefined();
+            expect(err).toBeTruthy();
+            done();
+          });
+        });
+    });
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
+const { version } = require('../../package.json');
 const _ = require('lodash');
 const mkdirp = require('mkdirp');
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
-const yosay = require('yosay');
 const Generator = require('yeoman-generator');
 
 // Common functions
 const utils = require('../../common/utils');
+
+const banner = chalk`
+{bold                          8                           } {red      8            w       }
+{bold Yb  db  dP .d8b. 8d8b .d88 88b. 8d8b .d88b d88b d88b } {red 88b. 8 8   8 .d88 w 8d8b. }
+{bold  YbdPYbdP  8' .8 8P   8  8 8  8 8P   8  dP  Yb.  Yb. } {red 8  8 8 8b d8 8  8 8 8P Y8 }
+{bold   YP  YP    Y8P' 8     Y88 88P' 8     Y88P Y88P Y88P } {red 88P' 8  Y8P8  Y88 8 8   8 }
+{bold                            8                         } {red 8            wwdP         }
+`;
 
 // The main generator is indipendent from all
 // it initiate the project creating also a configuration file
@@ -17,6 +25,8 @@ const utils = require('../../common/utils');
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
+
+    this.log(banner);
 
     // Optionally take first argument as project name
     this.argument('appname', {
@@ -39,9 +49,6 @@ module.exports = class extends Generator {
 
   // Ask user for project details
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to ' + chalk.bold.redBright('wordpress-plugin') + ' generator!'));
-
     // Main project setup prompt questions
     // this details are stored into yo-rc config file
     const prompts = [
@@ -51,45 +58,46 @@ module.exports = class extends Generator {
         message: 'What slug do you want to use for this project?',
         default: _.kebabCase(this.options.appname || this.appname),
         validate: utils.validateSlug
-      },
-      {
+      }, {
         name: 'projectTitle',
         message: 'What is the full name for this project?',
-        default: function (answers) {
-          return _.startCase(_.toLower(answers.projectName));
-        },
+        default: answers => _.startCase(_.toLower(answers.projectName)),
         validate: utils.validateRequired
-      },
-      {
+      }, {
         type: 'text',
         name: 'projectDescription',
         message: 'What is the project description?',
-        default: function (answers) {
-          return 'This is the ' + answers.projectTitle + ' description.';
-        }
-      },
-      {
+        default: answers => `This is the ${answers.projectTitle} description`
+      }, {
         type: 'list',
         name: 'projectManager',
         message: 'What do you want to use a project manager?',
         default: 'grunt',
-        choices: ['grunt', 'gulp'],
+        choices: [
+          {
+            name: 'Nothing',
+            value: false
+          }, {
+            name: 'Grunt',
+            value: 'grunt'
+          }, {
+            name: 'Gulp',
+            value: 'gulp'
+          }
+        ],
         validate: utils.validateProjectManager
-      },
-      {
+      }, {
         type: 'text',
         name: 'projectVersion',
         message: 'The version to initialize this project.',
         default: '0.0.1',
         validate: utils.validateVresion
-      },
-      {
+      }, {
         type: 'text',
         name: 'projectAuthor',
         message: 'The name of the author for this project?',
         default: this.user.git.name() || ''
-      },
-      {
+      }, {
         type: 'text',
         name: 'projectLicense',
         message: 'What license do you want to use?',
@@ -106,15 +114,11 @@ module.exports = class extends Generator {
 
   // Configure the project folder and context
   configuring() {
-    // Create the root folder if not exists
     if (path.basename(this.destinationPath()) !== this.props.projectName) {
-      this.log(
-        'Your generator must be inside a folder named ' + this.props.projectName + '\n' +
-        'I\'ll automatically create this folder.'
-      );
+      this.log('Your generator must be inside a folder named ' + this.props.projectName + '\n' + 'I\'ll automatically create this folder.');
 
       // Try to create the folder
-      if( !mkdirp.sync(this.props.projectName) ) {
+      if (!mkdirp.sync(this.props.projectName)) {
         // Exit with error since project root can't be created
       }
 
@@ -127,70 +131,144 @@ module.exports = class extends Generator {
     this.props.definePrefix = this.props.className.toUpperCase();
 
     // Init an empty repository
-    this.spawnCommandSync('git', ['init', '--quiet']);
+    if (this.options.git) {
+      this.spawnCommandSync('git', ['init', '--quiet']);
+    }
   }
 
   // Save the project details into configuration file
   savings() {
-    // Store the main project configurations
     this.config.set(this.props);
-    // Create a configuration file
     this.config.save();
   }
 
   // Compile and write the project files
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('plugin/include/*'),
-      this.destinationPath('include/'),
-      this.props
-    );
+    const copyFiles = [
+      {
+        src: this.templatePath('plugin/.*'),
+        dest: this.destinationPath()
+      }
+    ];
 
-    // Copy all dotfiles
-    this.fs.copy(
-      this.templatePath('plugin/.*'),
-      this.destinationPath()
-    );
+    const copyTplFiles = [
+      {
+        src: this.templatePath('plugin/_plugin.php'),
+        dest: this.destinationPath(this.props.projectName + '.php')
+      }, {
+        src: this.templatePath('plugin/readme.txt'),
+        dest: this.destinationPath('readme.txt')
+      }, {
+        src: this.templatePath('plugin/include/*'),
+        dest: this.destinationPath('include/')
+      }
+    ];
 
-    // Render and copy main plugin file
-    this.fs.copyTpl(
-      this.templatePath('plugin/_plugin.php'),
-      this.destinationPath(this.props.projectName + '.php'),
-      this.props
-    );
+    // Handle the different project managers configurations and tweaks
+    if (this.props.projectManager) {
+      const pkg = {
+        "name": this.props.projectName,
+        "version": this.props.projectVersion,
+        "description": this.props.projectDescription,
+        "author": this.props.projectAuthor,
+        "license": this.props.projectLicense,
+        "scripts": {
+          "start": "grunt",
+          "build": "grunt build",
+          "postinstall": "npm run build"
+        },
+        "devDependencies": {
+          "autoprefixer": "^7.1.4",
+          "babel-preset-es2015": "^6.24.1"
+        }
+      };
 
-    // Write the project manager files
-    switch (this.props.projectManager) {
-      case 'grunt':
-        this.fs.copyTpl(
-          this.templatePath('grunt/_package.json'),
-          this.destinationPath('package.json'),
-          this.props
-        );
-        this.fs.copy(
-          this.templatePath('grunt/_Gruntfile.js'),
-          this.destinationPath('Gruntfile.js')
-        );
-        break;
-      case 'gulp':
-        this.fs.copyTpl(
-          this.templatePath('gulp/_package.json'),
-          this.destinationPath('package.json'),
-          this.props
-        );
-        this.fs.copy(
-          this.templatePath('gulp/_gulpfile.js'),
-          this.destinationPath('gulpfile.js')
-        );
-        break;
+      switch (this.props.projectManager) {
+        // Write the grunt config file and extend dependencies
+        case 'grunt':
+          copyFiles.push({
+            src: this.templatePath('grunt/_Gruntfile.js'),
+            dest: this.destinationPath('Gruntfile.js')
+          }, {
+            src: this.templatePath('plugin/assets/src/scss'),
+            dest: this.destinationPath('assets/src/scss')
+          }, {
+            src: this.templatePath('plugin/assets/src/js'),
+            dest: this.destinationPath('assets/src/js')
+          });
+
+          // Extend default package configuration
+          pkg.devDependencies = Object.assign(pkg.devDependencies, {
+            "grunt": "^1.0.1",
+            "grunt-babel": "^7.0.0",
+            "grunt-contrib-clean": "^1.1.0",
+            "grunt-contrib-concat": "^1.0.1",
+            "grunt-contrib-jshint": "^1.1.0",
+            "grunt-contrib-uglify": "^3.1.0",
+            "grunt-contrib-watch": "^1.0.0",
+            "grunt-newer": "^1.3.0",
+            "grunt-phpdocs": "^0.1.0",
+            "grunt-postcss": "^0.9.0",
+            "grunt-sass": "^2.0.0",
+            "grunt-wp-i18n": "^1.0.0"
+          });
+          break;
+
+        // Write the gulp config file and extend dependencies
+        case 'gulp':
+          copyFiles.push({
+            src: this.templatePath('gulp/_gulpfile.js'),
+            dest: this.destinationPath('gulpfile.js')
+          }, {
+            src: this.templatePath('plugin/assets/src/scss'),
+            dest: this.destinationPath('assets/src/scss')
+          }, {
+            src: this.templatePath('plugin/assets/src/js'),
+            dest: this.destinationPath('assets/src/js')
+          });
+
+          // Extend default package configuration
+          pkg.devDependencies = Object.assign(pkg.devDependencies, {
+            "gulp": "^3.9.1",
+            "gulp-babel": "^6.1.2",
+            "gulp-concat": "^2.6.1",
+            "gulp-help": "^1.6.1",
+            "gulp-notify": "^2.2.0",
+            "gulp-rename": "^1.2.2",
+            "gulp-sass": "^3.1.0",
+            "gulp-uglify": "^2.0.0",
+            "gulp-watch": "^4.3.11",
+            "gulp-wp-pot": "^2.0.3",
+            "merge-stream": "^1.0.1"
+          });
+          break;
+      }
+
+      // Write the package file
+      this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+    } else {
+
+      copyFiles.push({
+        src: this.templatePath('plugin/assets/src/css'),
+        dest: this.destinationPath('assets/css')
+      }, {
+        src: this.templatePath('plugin/assets/src/js'),
+        dest: this.destinationPath('assets/js')
+      });
+
     }
+
+    // Simple copy files
+    copyFiles.forEach(o => this.fs.copy(o.src, o.dest));
+
+    // Render and copy files
+    copyTplFiles.forEach(o => this.fs.copyTpl(o.src, o.dest, this.props));
   }
 
   // Install project dependencies
   install() {
-    this.installDependencies({
-      bower: false,
-      npm: true
-    });
+    if (this.fs.exists(this.destinationPath('package.json'))) {
+      this.installDependencies({npm: true, bower: false, yarn: false});
+    }
   }
 };
